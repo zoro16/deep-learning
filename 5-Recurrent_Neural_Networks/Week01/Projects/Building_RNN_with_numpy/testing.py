999d7f299e104d59d6dc0ac1fbadea74246f9e2f
Mohamed Abdelgadir <mohamed.saleh16@gmail.com>
2018-02-28 17:42:25 +0800

    changed the folder name

--- 5-Recurrent_Neural_Networks/Week01/Projects/Building_RNN_with_numpy/testing.py
+++ 5-Recurrent_Neural_Networks/Week01/Projects/Building_RNN_with_numpy/testing.py
@@ -126,15 +126,15 @@ def rnn_forward(x, a0, parameters):
 
 
 
-np.random.seed(1)
-x = np.random.randn(3,10,4)
-a0 = np.random.randn(5,10)
-Waa = np.random.randn(5,5)
-Wax = np.random.randn(5,3)
-Wya = np.random.randn(2,5)
-ba = np.random.randn(5,1)
-by = np.random.randn(2,1)
-parameters = {"Waa": Waa, "Wax": Wax, "Wya": Wya, "ba": ba, "by": by}
+# np.random.seed(1)
+# x = np.random.randn(3,10,4)
+# a0 = np.random.randn(5,10)
+# Waa = np.random.randn(5,5)
+# Wax = np.random.randn(5,3)
+# Wya = np.random.randn(2,5)
+# ba = np.random.randn(5,1)
+# by = np.random.randn(2,1)
+# parameters = {"Waa": Waa, "Wax": Wax, "Wya": Wya, "ba": ba, "by": by}
 
 # rnn_forward(x, a0, parameters)
 
@@ -153,6 +153,9 @@ Instructions:
     1- Concatenate a⟨t−1⟩ and x⟨t⟩ in a single matrix: concat=[a⟨t−1⟩x⟨t⟩]
     2- Compute all the formulas 1-6. You can use sigmoid() (provided) and np.tanh().
     3- Compute the prediction y⟨t⟩. You can use softmax() (provided).
+
+https://stackoverflow.com/questions/30960412/concatenate-several-np-arrays-in-python
+https://docs.scipy.org/doc/numpy/reference/generated/numpy.concatenate.html
 '''
 
 def lstm_cell_forward(xt, a_prev, c_prev, parameters):
@@ -223,3 +226,33 @@ def lstm_cell_forward(xt, a_prev, c_prev, parameters):
     cache = (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters)
 
     return a_next, c_next, yt_pred, cache
+
+
+np.random.seed(1)
+xt = np.random.randn(3,10)
+a_prev = np.random.randn(5,10)
+c_prev = np.random.randn(5,10)
+Wf = np.random.randn(5, 5+3)
+bf = np.random.randn(5,1)
+Wi = np.random.randn(5, 5+3)
+bi = np.random.randn(5,1)
+Wo = np.random.randn(5, 5+3)
+bo = np.random.randn(5,1)
+Wc = np.random.randn(5, 5+3)
+bc = np.random.randn(5,1)
+Wy = np.random.randn(2,5)
+by = np.random.randn(2,1)
+
+parameters = {"Wf": Wf, "Wi": Wi, "Wo": Wo, "Wc": Wc, "Wy": Wy, "bf": bf, "bi": bi, "bo": bo, "bc": bc, "by": by}
+
+a_next, c_next, yt, cache = lstm_cell_forward(xt, a_prev, c_prev, parameters)
+
+# a_next, c_next, yt, cache = lstm_cell_forward(xt, a_prev, c_prev, parameters)
+# print("a_next[4] = ", a_next[4])
+# print("a_next.shape = ", c_next.shape)
+# print("c_next[2] = ", c_next[2])
+# print("c_next.shape = ", c_next.shape)
+# print("yt[1] =", yt[1])
+# print("yt.shape = ", yt.shape)
+# print("cache[1][3] =", cache[1][3])
+# print("len(cache) = ", len(cache))
